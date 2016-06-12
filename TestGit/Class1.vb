26b87197405bd0a3045d2159b34b0c62783fa660
Dominik1505 <dominik.weitz@kabelmail.de>
2016-06-12 20:35:07 +0200

    ps_show() -> Eingefügt

--- TestGit/Class1.vb
+++ TestGit/Class1.vb
@@ -1,3 +1,11 @@
 ﻿Public Class Class1
 
+    Public Sub ps_load()
+        Dim test As String = "Hello World"
+    End Sub
+
+    Public Sub ps_show()
+        Dim _integer As Integer = 2348393
+    End Sub
+
 End Class
